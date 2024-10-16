d382992aab6d39afbbcf8225e28137a240a97fb0
thjentruong <thjentruongtran@gmail.com>
2024-10-16 15:08:45 +0700

    lab4

---
+++ lab4-main/lib/main.dart
@@ -0,0 +1,52 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ngo Minh Phuc"),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: BallPage(),
+    );
+  }
+}
+
+class BallPage extends StatefulWidget {
+  const BallPage({super.key});
+
+  @override
+  State<BallPage> createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ballNum = 1;
+  void changeBall() {
+    setState(() {
+      ballNum = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+          onPressed: () {
+            changeBall();
+          },
+          child: Image(image: AssetImage("images/ball$ballNum.png"))),
+    );
+  }
+}
